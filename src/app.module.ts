2c51dfc604b9ca2891f2079816d824bb76c2ed1c
kishor <kishorgiri@lftechnology.com>
2024-03-28 10:18:20 +0545

    re arrange folder strucutre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AuthModule],
